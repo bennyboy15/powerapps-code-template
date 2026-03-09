96733a4334cdf191f0ab0d5b882ec76cb60e1861
Benjamin Harvey <benjaminharvey2000@gmail.com>
2026-03-09 19:12:44 +1030

    Component Library

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import TemplatePage from "./pages/TemplatePage.tsx";
-import SideNav from "./components/SideNav";
+import ComponentShowcase from "./pages/ComponentShowcase.tsx";
+import SideNav from "./components/NavBar.tsx";
 import "./App.css";
 
 function App() {
   return (
     <div className="flex">
-      <SideNav />
-      <div className="p-5">
+      <div className="fixed top-0 left-0">
+        <SideNav/>
+      </div>
+      <div className="p-5 mt-15">
         <Routes>
           <Route path="/" element={<TemplatePage />} />
+          <Route path="/showcase" element={<ComponentShowcase />} />
         </Routes>
       </div>
     </div>
