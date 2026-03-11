3e0de99919a697117d621f0968cf6bac8b1f2efa
Benjamin Harvey <73825010+bennyboy15@users.noreply.github.com>
2026-03-11 15:08:06 +1030

    Update routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-import TemplatePage from "./pages/TemplatePage.tsx";
 import ComponentShowcase from "./pages/ComponentShowcase.tsx";
 import SideNav from "./components/NavBar.tsx";
 import "./App.css";
+import DashboardPage from "./pages/DashboardPage.tsx";
+import SchedulingPage from "./pages/SchedulingPage.tsx";
+import AdminPage from "./pages/AdminPage.tsx"
 
 function App() {
   return (
-    <div className="flex">
-      <div className="fixed top-0 left-0">
+    <div className="flex bg-gray-50">
+      <div className="fixed top-0 left-0 z-50">
         <SideNav/>
       </div>
-      <div className="p-5 mt-15">
+      <div className="mt-16">
         <Routes>
-          <Route path="/" element={<TemplatePage />} />
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/schedule" element={<SchedulingPage />} />
+          <Route path="/admin" element={<AdminPage />} />
           <Route path="/showcase" element={<ComponentShowcase />} />
         </Routes>
       </div>
